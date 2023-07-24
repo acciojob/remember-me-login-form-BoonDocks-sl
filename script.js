62a1e7531474d16bcb1746e2f0e90741b84daf74
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-07-24 09:31:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-	// Retrieve stored username and password from local storage
-	const savedUsername = localStorage.getItem('username');
-	const savedPassword = localStorage.getItem('password');
+//your JS code here. If required.
+//your JS code here. If required.
+//your code here
+const form = document.querySelector("form");
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const rememberMeCheckbox = document.querySelector("#checkbox");
+const exisitingButton = document.querySelector("#existing");
 
-	// Check if there are saved details and show the existing user button
-	if (savedUsername && savedPassword) {
-		const existingUserButton = document.getElementById('existing');
-		existingUserButton.style.display = 'inline-block';
-	} else {
-		const existingUserButton = document.getElementById('existing');
-		existingUserButton.style.display = 'none';
-	}
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  const rememberMe = rememberMeCheckbox.checked;
 
-	// Handle form submission
-	document.getElementById('loginForm').addEventListener('submit', function(e) {
-		e.preventDefault();
+  if (rememberMe) {
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+  } else {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+  }
 
-		const username = document.getElementById('username').value;
-		const password = document.getElementById('password').value;
-		const rememberMe = document.getElementById('checkbox').checked;
+  alert(`Logged in as ${username}`);
+  showButton();
 
-		// Store username and password if "Remember me" checkbox is checked
-		if (rememberMe) {
-			localStorage.setItem('username', username);
-			localStorage.setItem('password', password);
-		} else {
-			// Remove stored username and password
-			localStorage.removeItem('username');
-			localStorage.removeItem('password');
-		}
-
-		alert(`Logged in as ${username}`);
-	});
-
-	// Handle existing user button click
-	document.getElementById('existing').addEventListener('click', function() {
-		const savedUsername = localStorage.getItem('username');
-		alert(`Logged in as ${savedUsername}`);
-	});
+  form.reset(); // Clear the form fields
 });
+
+function loginAsSaved() {
+  const username = localStorage.getItem("username");
+  const password = localStorage.getItem("password");
+
+  if (username && password) {
+    alert(`Logged in as ${username}`);
+    form.reset();
+  }
+}
+
+function showButton() {
+  const username = localStorage.getItem("username");
+  const password = localStorage.getItem("password");
+
+  if (username && password) {
+    exisitingButton.style.display = "block";
+  } else {
+    exisitingButton.style.display = "none";
+  }
+}
+
+exisitingButton.style.display = "none";
+exisitingButton.addEventListener("click", loginAsSaved);
+showButton();
